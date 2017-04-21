7f5bf5433e211754699c5e8e391b154649f84b9a
Opeoluwa <oolakan@yahoo.com>
2017-04-21 13:26:07 +0100

    master updated

--- src/app.js
+++ src/app.js
@@ -313,8 +313,8 @@ class FacebookBot {
             const locationLat = messageAttachments[0].payload.coordinates.lat;
             const locationLong = messageAttachments[0].payload.coordinates.long;
             let locationResult = getAllLocation(locationLat, locationLong)
-            this.doTextResponse(sender, JSON.stringify(locationResult));
-            // sendTextMessage(senderID, locationLat + ", " + locationLong);
+                //this.doTextResponse(sender, JSON.stringify(locationResult));
+                // sendTextMessage(senderID, locationLat + ", " + locationLong);
         }
     }
 
