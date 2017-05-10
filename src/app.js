ef88720ebfcbecd1ad5a48ec60bd672f670bab33
Opeoluwa <oolakan@yahoo.com>
2017-05-10 16:06:20 +0100

    master updated

--- src/app.js
+++ src/app.js
@@ -464,16 +464,16 @@ function customerOnboarded() {
 
     // return new Promise(function(resolve, reject) {
     soap.createClient(url, function(err, client) {
-        //if (err) throw new Error(err);
+        if (err) throw new Error(err);
         let args = {
-            UserId: userId,
-            hash: sha512(userId)
-        }
-        console.log('result expected from wsdl' + args.toString());
+                UserId: userId,
+                hash: sha512(userId)
+            }
+            //console.log('result expected from wsdl' + args.toString());
         client.Service1.BasicHttpBinding_IService1.CustomerOnboarded(args, function(err, result) {
             // result is a javascript object
-            console.log('result expected from wsdl' + args.toString());
-            console.log(result);
+            console.log(userId);
+            //console.log(result);
         });
     });
     // });
