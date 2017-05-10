eea9a59f932aa79c092740e3694cd1740b3fb937
Opeoluwa <oolakan@yahoo.com>
2017-05-10 15:50:31 +0100

    master updated

--- src/app.js
+++ src/app.js
@@ -461,6 +461,7 @@ class FacebookBot {
 //consume soap service
 //consume service to verify onboarded member
 function customerOnboarded() {
+
     // return new Promise(function(resolve, reject) {
     soap.createClient(url, function(err, client) {
         if (err) throw new Error(err);
@@ -468,6 +469,7 @@ function customerOnboarded() {
             UserId: userId,
             hash: sha512(userId)
         }
+        console.log('result expected from wsdl' + args.toString());
         client.Service1.BasicHttpBinding_IService1.CustomerOnboarded(args, function(err, result) {
             // result is a javascript object
             console.log('result expected from wsdl' + args.toString());
